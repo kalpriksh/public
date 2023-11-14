5caaa54df735bf1dd8b1e7fca75d640106083579
kalpriksh <kalprikshbist@gmail.com>
2023-11-14 10:47:08 +0530

    add init resume changes

---
+++ StreamLit_resume/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from PIL import Image
+
+# creating a resume using streamLit
+
+with st.sidebar:
+    image = Image.open('image.jpg')
+    st.image(image)
+'''
+# Hey there! I am Kalpriksh
+
+'''
+
+'''
+-----
+# About Me
+'''
+
+'''
+---
+# Education and Qualifications
+..if that matters :sweat_smile:
+
+'''
+
+
+
+
+
